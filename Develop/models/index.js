ce9663c96d6101ee43f913e00c8f68ffe879abfb
Devin Polichetti <devinpolichetti@gmail.com>
2023-03-31 11:53:42 -0400

    added

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -9,25 +9,16 @@ Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
 // Categories have many Products
-Product.belongsTo(Category, {
+Category.hasMany(Product, {
   foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(
   Tag,
-  { through: ProductTag },
-  {
-    foreignKey: "product_id",
-  }
+  { foreignKey: "product_id", through: ProductTag },
 );
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(
-  Product,
-  { through: ProductTag },
-  {
-    foreignKey: "tag_id",
-  }
-);
+Tag.belongsToMany(Product, { foreignKey: "tag_id", through: ProductTag });
 
 module.exports = {
   Product,
